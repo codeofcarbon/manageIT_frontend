d9606b8c86b994012b9c9df7e435ee842c481939
Igor Nowakowski <igornowakowski24@wp.pl>
2021-07-21 18:30:46 +0200

    First components works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { VerticalNavbarComponent } from './vertical-navbar/vertical-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainComponent,
+    FooterComponent,
+    VerticalNavbarComponent
   ],
   imports: [
     BrowserModule

6858c3d88bfca452a2836b2a27566381bf79f145
Igor Nowakowski <igornowakowski24@wp.pl>
2021-08-19 13:49:53 +0200

    Fixed slow app working problem (cookies weren't send in the request's and that was a problem)

--- src/app/http_interceptor/http-interceptor.ts
+++ src/app/http_interceptor/http-interceptor.ts
@@ -8,17 +8,14 @@ import { Observable } from "rxjs";
 export class BasicAuthInterceptor implements HttpInterceptor {
     
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if( req.url === 'http://localhost:8080/api/v1/users' && req.method === 'POST') {
+        if( req.url === 'http://localhost:8080/api/v1/users/authenticate' && req.method === 'GET') {
             console.log('Nothing to intercept')
             return next.handle(req)
         }
-        const user = JSON.parse(localStorage.getItem('currentUser'))
-        console.log(user)
-        
-        const auth = req.clone({
-            headers: req.headers.set('Authorization', 'Basic ' + btoa(`${user.username}:${user.password}`))
+        req = req.clone({
+            withCredentials: true
         });
-        return next.handle(auth)
+        return next.handle(req)
 
     }
 
